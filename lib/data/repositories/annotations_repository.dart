aa6a4c29fb3798bef215ef09c5ade94da7ae50a4
Zolani Mjikeliso <zolanimjikeliso20@gmail.com>
2026-06-22 10:48:33 +0200

    feat: migrate annotations repository to fetch from live firestore collections

--- lib/data/repositories/annotations_repository.dart
+++ lib/data/repositories/annotations_repository.dart
@@ -1,9 +1,34 @@
 import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import '../models/annotations_model.dart'; 
 
 class AnnotationRepository {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   Future<List<AnnotationModel>> getAnnotations(String slideId) async {
+    try {
+      DocumentSnapshot doc = await _firestore.collection('annotations').doc(slideId).get();
+
+      if (!doc.exists || doc.data() == null) {
+        return [];
+      }
+
+      final data = doc.data() as Map<String, dynamic>;
+
+      if (data['annotations'] == null) {
+        return [];
+      }
+      return (data['annotations'] as List)
+      .map((e) => AnnotationModel.fromJson(e))
+      .toList();
+    } catch (e) {
+      print('Error fetching annotations from Firestore: $e');
+      return [];
+    }
+  }
+}
+/*   Future<List<AnnotationModel>> getAnnotations(String slideId) async {
     final jsonString = await rootBundle.loadString('assets/data/annotations.json');
     final List<dynamic> data = json.decode(jsonString);
 
@@ -19,5 +44,4 @@ class AnnotationRepository {
     return (slideData['annotations'] as List)
         .map((e) => AnnotationModel.fromJson(e))
         .toList();
-  }
-}
+  } */
